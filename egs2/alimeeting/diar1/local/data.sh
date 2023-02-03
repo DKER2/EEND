febe4f6854844434523301af72e3171052405a2f
HuyPhuong <pdanghuy03@gmail.com>
2023-02-03 18:01:12 +0800

    Download Train and Eval

--- egs2/alimeeting/diar1/local/data.sh
+++ egs2/alimeeting/diar1/local/data.sh
@@ -15,7 +15,7 @@ fs=8k
 textgrid_dir=downloads/Eval_Ali/Eval_Ali_far/textgrid_dir
 wav_dir=downloads/Eval_Ali/Eval_Ali_far/audio_dir
 work_dir=downloads/Eval_Ali/Eval_Ali_far
-output_dir=data/val
+output_dir=data/
 
  . utils/parse_options.sh || exit 1;
 
@@ -38,34 +38,41 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ] ; then
     wget -c --tries=0 --read-timeout=20 https://speech-lab-share-data.oss-cn-shanghai.aliyuncs.com/AliMeeting/openlr/Eval_Ali.tar.gz -P $ALIMEETING
     tar -xf $ALIMEETING/Eval_Ali.tar.gz
     rm -rf $ALIMEETING/Eval_Ali.tar.gz
+
+    wget -c --tries=0 --read-timeout=20 https://speech-lab-share-data.oss-cn-shanghai.aliyuncs.com/AliMeeting/openlr/Train_Ali_far.tar.gz -P $ALIMEETING
+    tar -xf $ALIMEETING/Train_Ali_far.tar.gz
+    rm -rf $ALIMEETING/Train_Ali_far.tar.gz
 fi
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ] ; then
-mkdir -p $output_dir
-# Prepare the AliMeeting data
-echo "Prepare Alimeeting data"
-find $wav_dir -name "*\.wav" > $output_dir/wavlist
-sort  $output_dir/wavlist > $output_dir/tmp
-cp $output_dir/tmp $output_dir/wavlist
-awk -F '/' '{print $NF}' $output_dir/wavlist | awk -F '.' '{print $1}' > $output_dir/uttid
-paste $output_dir/uttid $output_dir/wavlist > $output_dir/wav.scp
-paste $output_dir/uttid $output_dir/uttid > $output_dir/utt2spk
-cp $output_dir/utt2spk $output_dir/spk2utt
-cp $output_dir/uttid $output_dir/text
+    for name in train eval; do
+        output_task_dir = "$output_dir/$name"
+        mkdir -p $output_task_dir
+        # Prepare the AliMeeting data
+        echo "Prepare Alimeeting data"
+        find $wav_dir -name "*\.wav" > $output_task_dir/wavlist
+        sort  $output_task_dir/wavlist > $output_task_dir/tmp
+        cp $output_task_dir/tmp $output_task_dir/wavlist
+        awk -F '/' '{print $NF}' $output_task_dir/wavlist | awk -F '.' '{print $1}' > $output_task_dir/uttid
+        paste $output_task_dir/uttid $output_task_dir/wavlist > $output_task_dir/wav.scp
+        paste $output_task_dir/uttid $output_task_dir/uttid > $output_task_dir/utt2spk
+        cp $output_task_dir/utt2spk $output_task_dir/spk2utt
+        cp $output_task_dir/uttid $output_task_dir/text
 
-echo "Process textgrid to obtain rttm label"
-find -L $textgrid_dir -iname "*.TextGrid" >  $output_dir/textgrid.flist
-sort  $output_dir/textgrid.flist  > $output_dir/tmp
-cp $output_dir/tmp $output_dir/textgrid.flist 
-paste $output_dir/uttid $output_dir/textgrid.flist > $output_dir/uttid_textgrid.flist
-while read text_file
-do  
-    text_grid=`echo $text_file | awk '{print $1}'`
-    echo $text_grid
-    text_grid_path=`echo $text_file | awk '{print $2}'`
-    python local/make_textgrid_rttm.py --input_textgrid_file $text_grid_path \
-                                        --uttid $text_grid \
-                                        --output_rttm_file $output_dir/${text_grid}.rttm
-done < $output_dir/uttid_textgrid.flist
-cat $output_dir/*.rttm > $output_dir/rttm
+        echo "Process textgrid to obtain rttm label"
+        find -L $textgrid_dir -iname "*.TextGrid" >  $output_task_dir/textgrid.flist
+        sort  $output_task_dir/textgrid.flist  > $output_task_dir/tmp
+        cp $output_task_dir/tmp $output_task_dir/textgrid.flist 
+        paste $output_task_dir/uttid $output_task_dir/textgrid.flist > $output_task_dir/uttid_textgrid.flist
+        while read text_file
+        do  
+            text_grid=`echo $text_file | awk '{print $1}'`
+            echo $text_grid
+            text_grid_path=`echo $text_file | awk '{print $2}'`
+            python local/make_textgrid_rttm.py --input_textgrid_file $text_grid_path \
+                                                --uttid $text_grid \
+                                                --output_rttm_file $output_task_dir/${text_grid}.rttm
+        done < $output_task_dir/uttid_textgrid.flist
+        cat $output_task_dir/*.rttm > $output_task_dir/rttm
+    done
 fi
